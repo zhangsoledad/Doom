ee15531de319683b22ec43ab4aec96cb4dd9db8b
张顶炜 <zhangdingwei@souche.com>
2016-03-28 12:08:45 +0800

    release host

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :doom, Doom.Endpoint,
   http: [port: 4000],
+  url: [host: "doom.sqaproxy.souche.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
